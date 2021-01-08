7703c8f0b31b037337513264b2a7145ca30e4b73
Saiyam Shah <saiyamjain88@gmail.com>
2021-01-08 15:04:59 +0530

    Crud operation through postman

--- src/main/java/com/example/demo/demo4controller.java
+++ src/main/java/com/example/demo/demo4controller.java
@@ -1,12 +1,21 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class demo4controller {
 
 	@Autowired
@@ -27,16 +36,71 @@ public class demo4controller {
 		return "form.jsp";
 	}
 	
-	@RequestMapping("/getdata")
-	public ModelAndView getdetails(@RequestParam int id)
+//	@RequestMapping("/getdata")
+//	public ModelAndView getdetails(@RequestParam int id)
+//	{
+//		ModelAndView mv = new ModelAndView("getform.jsp");
+//		
+//		demoform4 demo = repo.findById(id).orElse(new demoform4());		
+//		
+//		
+//		System.out.println(repo.findByName("saiyam"));
+//	//	System.out.println(repo.findByNameSorted("saiyam"));
+//		
+//		mv.addObject(demo);
+//		
+//		return mv;		
+//	}
+//	
+	
+	
+	
+	
+	@GetMapping("/details")
+	public List<demoform4> getdetails()
 	{
-		ModelAndView mv = new ModelAndView("getform.jsp");
-		
-		demoform4 demo = repo.findById(id).orElse(new demoform4());		
-		mv.addObject(demo);
-		
-		return mv;		
+		return repo.findAll();
 	}
+	
+	
+	@DeleteMapping("/details/{id}")
+	public String deletedetails(@PathVariable int id)
+	{
+		
+		repo.deleteById(id);
+		
+		return "deleted";
+	}
+	
+
+	@PutMapping(path = "/details" , consumes = "application/json")
+	public demoform4 saveorupdatedetails(@RequestBody demoform4 demo)
+	{
+		repo.save(demo);
+		return demo;
+	}
+	
+	
+	
+	
+	
+	@PostMapping("/details")
+	public demoform4 adddatapostman(@RequestBody demoform4 demo)
+	{
+		repo.save(demo);
+		return demo;
+	}
+	
+	
+	
+	
+	@RequestMapping("/details/{id}")
+	@ResponseBody
+	public Optional<demoform4> getdetails(@PathVariable("id") int id)
+	{
+		return repo.findById(id);
+	}
+	
 	
 	
 	@RequestMapping("/deleterecord")
--- src/main/java/com/example/demo/formrepo.java
+++ src/main/java/com/example/demo/formrepo.java
@@ -1,10 +1,19 @@
 package com.example.demo;
 
-import org.springframework.data.repository.CrudRepository;
+import java.util.List;
 
-public interface formrepo extends CrudRepository<demoform4,Integer> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface formrepo extends JpaRepository<demoform4,Integer> {
 
 	
+	List<demoform4> findByName(String name);
 	
+	
+	
+	@Query("from demoform4 order by name")
+	List<demoform4> findByNameSorted(String name);
+//	
 	
 }
